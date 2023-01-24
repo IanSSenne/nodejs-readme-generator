2dfd17768037b1c641ebcd8918433e9ba6e0359e
Ian Senne <ianssenne@gmail.com>
2023-01-23 19:08:56 -0800

    finalize implimentation, replace boilerplate markdown generation code with custom class

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // [x]TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs/promises");
-const generateMarkdown = require("./utils/generateMarkdown");
+const axios = require("axios");
+const MDDocument = require("./utils/generateMarkdown");
 
 // [x]TODO: Create an array of questions for user input
 const questions = [
@@ -35,13 +36,13 @@ const questions = [
 		type: "list",
 		name: "repo.license",
 		message: "What kind of license should your project have?",
-		choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+		choices: [],
 	},
 	{
 		type: "input",
 		name: "repo.tests",
 		message: "What command should be run to run tests?",
-		default: "npm test",
+		default: "npm run test",
 	},
 	{
 		type: "input",
@@ -56,13 +57,46 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {
-	inquirer.prompt(questions).then((answers) => {
-		console.log(answers);
-	});
+async function init() {
+	const licenses = await axios.get("https://api.github.com/licenses").then((res) => res.data)
+	questions[5].choices.push(...licenses.map((license) => license.name));
+
+	const response = (await inquirer.prompt(questions));
+	response.repo.license = licenses.find((license) => license.name === response.repo.license);
+	let licenseMeta = (await axios.get(response.repo.license.url)).data.body;
+	let md = new MDDocument();
+	md.header(response.repo.name, 1);
+	md.badge("License", response.repo.license.spdx_id, "blue",{style:"plastic"});
+	md.p(response.repo.description);
+	md.header("Table of Contents", 2);
+	md.list([
+		MDDocument.link("Installation", "#installation"),
+		MDDocument.link("Usage", "#usage"),
+		MDDocument.link("Contributing", "#contributing"),
+		MDDocument.link("Tests", "#tests"),
+		MDDocument.link("Questions", "#questions"),
+		MDDocument.link("License", "#license"),
+	]);
+	md.header("Installation", 2);
+	md.p(MDDocument.inlineCode(response.repo.installation));
+	md.header("Usage", 2);
+	md.p(response.repo.usage);
+	md.header("Contributing", 2);
+	md.p(response.repo.contributing);
+	md.header("Tests", 2);
+	md.p(MDDocument.inlineCode(response.repo.tests));
+	md.header("Questions", 2);
+	md.p("If you have any questions about the repo, open an issue or contact me directly at " +
+		MDDocument.link(response.repo.email,"mailto:"+response.repo.email)+ ".");
+	md.header("License", 2);
+	md.p(MDDocument.bold(response.repo.license.name));
+	md.code(licenseMeta);
+	writeToFile("README.md", md.toString());
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,53 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+class MDDocument {
+	constructor() {
+		this.document = [];
+	}
+	
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+	static bold(content) {
+		return "**" + content + "**";
+	}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-/**
- * 
- * @param {{name:string;description:string;installation:string;usage:string;contributing:string;license:string;tests:string;username:string;email:string;}} data 
- * @returns 
- */
-function generateMarkdown(data) {
-  return `# ${data.name}
-
-`;
+	badge(label, message, color, params = {}) {
+		let url = new URLSearchParams({
+			label,message,color,...params
+		})
+		this.document.push(`![${label} - ${message}](https://img.shields.io/static/v1?${url.toString()})`);
+	}
+	header(content, size = 1) {
+		this.document.push("#".repeat(size) + " " + content);
+	}
+	p(content) {
+		this.document.push(String(content));
+	}
+	link(content,href) {
+		this.document.push(`[${content}](${href})`);
+	}
+  static link( content,href) {
+    return `[${content}](${href})`;
+  }
+	imgLink(content,href) {
+		this.document.push(`![${content}](${href})`);
+	}
+	code(content, language = "") {
+		this.document.push("```" + language);
+		this.document.push(content);
+		this.document.push("```");
+	}
+	static inlineCode(content) {
+		return "`" + content + "`";
+  }
+  list(items) {
+    this.document.push(items.map(item => "- " + item).join("\n"));
+  }
+  table(headers, rows) {
+    this.document.push(headers.join(" | "));
+    this.document.push(headers.map(() => "---").join(" | "));
+    this.document.push(...rows.map(row => row.join(" | ")));
+  }
+  toString() {
+    return this.document.join("\n\n");
+  }
 }
 
-module.exports = generateMarkdown;
+module.exports = MDDocument;
